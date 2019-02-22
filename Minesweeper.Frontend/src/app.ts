fbf7e42cdf69d5d5b70a20d1039e7c00eeaa0814
BalazsArva <balazs.arva@windowslive.com>
2019-02-22 13:54:52 +0100

    Home and new game pages for aurelia frontend.

--- Minesweeper.Frontend/src/app.ts
+++ Minesweeper.Frontend/src/app.ts
@@ -1,5 +1,33 @@
-import {Aurelia} from 'aurelia-framework';
-import {PLATFORM} from 'aurelia-pal';
+import { Aurelia } from 'aurelia-framework';
+import { PLATFORM } from 'aurelia-pal';
+import { Router, RouterConfiguration, NavigationInstruction, PipelineStep, Next, Redirect } from 'aurelia-router';
+import { inject } from 'aurelia-framework';
+import environment from './environment';
+
 export class App {
-  public message: string = 'Hello World!';
+  router: Router = null;
+  message = "Hello world";
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'DishPlanner';
+
+    config.map([
+      {
+        route: '',
+        moduleId: PLATFORM.moduleName('./pages/home'),
+        title: 'Home',
+        name: 'home'
+      },
+      {
+        route: 'games/new',
+        moduleId: PLATFORM.moduleName('./pages/new-game'),
+        title: 'New game',
+        name: 'newgame'
+      },
+    ]);
+
+    config.fallbackRoute('');
+
+    this.router = router;
+  }
 }
