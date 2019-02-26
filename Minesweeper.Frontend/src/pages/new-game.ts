fc7c2a08d8091b7b2ce24e7940d4abb851ba7959
BalazsArva <balazs.arva@windowslive.com>
2019-02-26 11:59:30 +0100

    Fixed difficulty selection in new game screen.

--- Minesweeper.Frontend/src/pages/new-game.ts
+++ Minesweeper.Frontend/src/pages/new-game.ts
@@ -3,19 +3,14 @@ import { Difficulty } from '../interfaces/difficulty';
 
 export class NewGame {
 
-  @bindable isSelected: boolean = false;
   @bindable displayName: string = null;
   @bindable selectedDifficulty: Difficulty = null;
-  @bindable difficulties: Difficulty[] = [
+  @bindable selectableDifficulties: Difficulty[] = [
     { width: 10, height: 10, mines: 10, title: 'Small' },
     { width: 15, height: 25, mines: 40, title: 'Medium' },
     { width: 16, height: 32, mines: 50, title: 'Large' }
   ];
 
   constructor() {
-  }
-
-  selectDifficulty(newDifficulty: Difficulty) {
-    this.selectedDifficulty = newDifficulty;
   }
 }
--- Minesweeper.Frontend/src/resources/elements/difficulty-selector.ts
+++ Minesweeper.Frontend/src/resources/elements/difficulty-selector.ts
@@ -1,8 +1,13 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, bindingMode } from 'aurelia-framework';
 import { Difficulty } from '../../interfaces/difficulty';
 
 export class DifficultySelector {
   @bindable difficulty: Difficulty = null;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) selectedDifficulty: Difficulty = null;
   @bindable inputGroup: string = 'difficulty';
+
+  get isSelected(): boolean {
+    return this.difficulty !== null && this.difficulty === this.selectedDifficulty;
+  }
 }
 
