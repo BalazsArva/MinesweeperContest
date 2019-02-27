3ffc25587b35a83ed181dd43c3ad5bbfc105dbbd
BalazsArva <balazs.arva@windowslive.com>
2019-02-27 13:53:39 +0100

    Initial endpoint for making a move.

---
+++ Minesweeper.Backend/Minesweeper.WebAPI/Contracts/Requests/MakeMoveRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Minesweeper.WebAPI.Contracts.Requests
+{
+    public class MakeMoveRequest
+    {
+        public string PlayerId { get; set; }
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+    }
+}
