5f46598a22c23e4a9c68315d8a0439bc608f252e
Árva Balázs <balazs.arva@windowslive.com>
2019-03-03 16:28:26 +0100

    IdentityServer db schema migrations, primitive config.

---
+++ Minesweeper.Backend/Minesweeper.Identity/Data/MinesweeperIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Minesweeper.Identity.Data
+{
+    public class MinesweeperIdentityDbContext : IdentityDbContext
+    {
+        public MinesweeperIdentityDbContext(DbContextOptions<MinesweeperIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Minesweeper.Backend/Minesweeper.Identity/Startup.cs
+++ Minesweeper.Backend/Minesweeper.Identity/Startup.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Minesweeper.Identity.Data;
 
 namespace Minesweeper.Identity
 {
     public class Startup
     {
+        private static readonly string migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +24,34 @@ namespace Minesweeper.Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MinesweeperIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Minesweeper.IdentityDb")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<MinesweeperIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddIdentityServer()
+                // TODO: Replace with a proper signing credential later
+                .AddDeveloperSigningCredential()
+                .AddConfigurationStore(options =>
+                {
+                    // this adds the config data from DB (clients, resources)
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(Configuration.GetConnectionString("Minesweeper.ConfigDb"), sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    // this adds the operational data from DB (codes, tokens, consents)
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(Configuration.GetConnectionString("Minesweeper.OperationalDb"), sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                })
+                .AddAspNetIdentity<IdentityUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +66,8 @@ namespace Minesweeper.Identity
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseIdentityServer();
 
             app.UseHttpsRedirection();
             app.UseMvc();
