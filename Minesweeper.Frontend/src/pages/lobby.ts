ea6ad8ebc3d6fa4e54fc192987de458274c90c91
Árva Balázs <balazs.arva@windowslive.com>
2019-03-10 17:38:22 +0100

    Upon joining a game in the lobby, the player is redirected to the game's page.

--- Minesweeper.Frontend/src/pages/lobby.ts
+++ Minesweeper.Frontend/src/pages/lobby.ts
@@ -2,6 +2,7 @@ import { autoinject } from "aurelia-framework";
 
 import { LobbyService, GetAvailableGamesResult, GetAvailableGamesResponse, AvailableGame } from "services/lobby-service";
 import { GameService } from "services/game-service";
+import { Router } from "aurelia-router";
 
 
 @autoinject
@@ -10,7 +11,7 @@ export class Lobby {
     total: number = 0;
     availableGames: AvailableGame[] = [];
 
-    constructor(private lobbyService: LobbyService, private gameService: GameService) {
+    constructor(private lobbyService: LobbyService, private gameService: GameService, private router: Router) {
     }
 
     async activate() {
@@ -28,6 +29,9 @@ export class Lobby {
     }
 
     async joinGame(gameId: string) {
+        // TODO: Validate success before redirecting
         await this.gameService.joinGame(gameId);
+
+        return this.router.navigateToRoute("game", { gameId });
     }
 }
